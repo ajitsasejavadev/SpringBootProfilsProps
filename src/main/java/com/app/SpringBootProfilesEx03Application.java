4cade8fbde1f5c282e39d21723a4ce2ad99ede19
ajitsase <34818235+ajitsase@users.noreply.github.com>
2019-06-10 19:42:29 +0530

    Here use multiple application-<profname>.properties and test with runner

---
+++ src/main/java/com/app/SpringBootProfilesEx03Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesEx03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesEx03Application.class, args);
+	}
+
+}
